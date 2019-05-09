88c41bf1e3e0f88f4ef3fdf43af8690cf77ef9f9
senopati-pn <senopati8821@gmail.com>
2019-05-09 21:31:28 +0700

    [UPDATE] meta 2

--- application/views/V_berita_detail.php
+++ application/views/V_berita_detail.php
@@ -12,30 +12,24 @@
     <link rel="stylesheet" href="assets_2/css/style.css">
 
       <!-- HTML Meta Tags -->
-    <meta name="description" content="cek">
+    <meta name="description" content="berita detail">
 
     <!-- Google / Search Engine Tags -->
     <meta itemprop="name" content="Masjid Pathok Nagari | Official Website">
-    <meta itemprop="description" content="Masjid Ad-Darojat Kauman Babadan adalah salah satu masjid Pathok Nagari
-    			   yang didirikan oleh Sultan Hamengku Buwono I pada tahun 1774 di atas tanah mutihan atau Sultan ground seluas 120 meter persegi.
-    			 ">
+    <meta itemprop="description" content="berita detail">
     <meta itemprop="image" content="https://pathoknagari.id/assets/images/pn1-transparent.png">
 
     <!-- Facebook Meta Tags -->
     <meta property="og:url" content="https://pathoknagari.id">
     <meta property="og:type" content="website">
     <meta property="og:title" content="Masjid Pathok Nagari | Official Website">
-    <meta property="og:description" content="Masjid Ad-Darojat Kauman Babadan adalah salah satu masjid Pathok Nagari
-    			   yang didirikan oleh Sultan Hamengku Buwono I pada tahun 1774 di atas tanah mutihan atau Sultan ground seluas 120 meter persegi.
-    			 ">
+    <meta property="og:description" content="berita detail">
     <meta property="og:image" content="https://pathoknagari.id/assets/images/pn1-transparent.png">
 
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Masjid Pathok Nagari | Official Website">
-    <meta name="twitter:description" content="Masjid Ad-Darojat Kauman Babadan adalah salah satu masjid Pathok Nagari
-    			   yang didirikan oleh Sultan Hamengku Buwono I pada tahun 1774 di atas tanah mutihan atau Sultan ground seluas 120 meter persegi.
-    			 ">
+    <meta name="twitter:description" content="berita detail">
     <meta name="twitter:image" content="https://pathoknagari.id/assets/images/pn1-transparent.png">
 
     <!-- Meta Tags Generated -->
