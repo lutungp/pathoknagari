ea3b6913324bbae0741d1fdc82152502a14a9e7f
senopati-pn <senopati8821@gmail.com>
2019-05-09 21:34:03 +0700

    test

--- application/views/V_berita_detail.php
+++ application/views/V_berita_detail.php
@@ -325,11 +325,11 @@
           FB.ui({
             method: 'share_open_graph',
             display: 'popup',
-            //href: 'https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>'
-            action_type: 'og.likes',
-            action_properties: JSON.stringify({
-              object:'https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>',
-            })
+            href: 'https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>'
+            // action_type: 'og.likes',
+            // action_properties: JSON.stringify({
+            //   object:'https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>',
+            // })
           }, function(response){});
       }
 
