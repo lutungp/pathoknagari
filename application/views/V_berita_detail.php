0c94bb30bcd61d3f0207fed54b586d11f5161041
senopati-pn <senopati8821@gmail.com>
2019-05-10 19:44:17 +0700

    [UPDATE]

--- application/views/V_berita_detail.php
+++ application/views/V_berita_detail.php
@@ -45,6 +45,10 @@
           top: 40px;
         }
 
+        .mobile {
+          display: none;
+        }
+
         @media (max-width: 700px) {
            .no-mobile {
              display: none;
@@ -139,11 +143,12 @@
                       </p>
 
                       <div class="profile-authors heading_space">
-                        <div class="" style="margin-bottom: 10px;">
+                        <div class="row" style="margin-bottom: 10px;">
                           <button id="shareFbBtn" type="button" class="btn btn-facebook"><i class="fa fa-facebook fa-2"></i></button>
-                          <a type="button" class="mobile btn btn-facebook" style="display:none;" href="whatsapp://send?text=https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>" data-action="share/whatsapp/share">
+                          &nbsp;
+                          <button type="button" class="mobile btn btn-facebook" href="whatsapp://send?text=https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>" data-action="share/whatsapp/share">
                             <i class="fa fa-whatsapp fa-2"></i>
-                          </a>
+                          </button>
                           <a type="button" class="no-mobile btn btn-facebook" href="https://web.whatsapp.com/send?text=https://pathoknagari.id/berita-detail?message=<?php echo $berita_id ?>" data-action="share/whatsapp/share">
                             <i class="fa fa-whatsapp fa-2"></i>
                           </a>
